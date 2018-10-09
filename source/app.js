746c8b1c8dd374746cd0fa0f8f699afd2c91d121
KubaLargo <Largoxpoint@gmail.com>
2018-10-09 18:36:15 +0200

    Update app.js
    poprawienie skryptu dla aplikacji

--- source/app.js
+++ source/app.js
@@ -12,6 +12,16 @@ app.get('/users', function(req, res){
 	console.log('Zażądano strony Users');
 });
 
+app.get('/services', function(req, res){
+	res.send('DevOps - strona Services!');
+	console.log('Zażądano strony Services');
+});
+
+app.get('/info', function(req, res){
+	res.send('DevOps - strona Info!');
+	console.log('Zażądano strony Info');
+});
+
 app.listen(3001, function(){
 	console.log('Aplikacja OK');
 }); 
